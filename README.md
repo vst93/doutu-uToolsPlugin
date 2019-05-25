02a108f66bae5ba9bfa1adcbfb137f8e529f9789
vst <vst93g@gmail.com>
2019-05-25 10:58:02 +0800

    完善gif动图复制

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 方便查找斗图表情，回车搜索后，点击看中的就复制到剪切板了
 
-数据来源 https://www.doutula.com/
+![](https://s2.ax1x.com/2019/05/24/VFKs0I.png)
+
+项目地址：https://github.com/vst93/utools-doutuPlugin
+
+数据来源：https://www.doutula.com/
+
+下载地址：https://pan.baidu.com/s/1HZ-hKXR5IKA3MPHsCYpQ5A 提取码: eqei
+
+### 更新
+`v.0.1.2`
+修改图片来源
+
+`v.0.1.4`
+修改图片复制方式，使得 gif 可以正常复制粘贴
--- preload.js
+++ preload.js
@@ -5,90 +5,80 @@ const path = require('path')
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
+const os = require('os');
 
-copyImg = (imgUrl) => {
+copyImg = imgUrl => {
 
-    // $.ajax(imgUrl, {
-    //     xhrFields: {
-    //         responseType: 'blob'    // 指定响应数据类型为blob格式
-    //     }
-    // }) 
-    // .then(blob => {
-    //     let reader = new FileReader(); 
-    //     reader.onloadend = function(){
-    //         let dataUrl = reader.result;
-    //         console.log("rr:"+dataUrl); // 输出DataURL数据
-    //         let img = electron.nativeImage.createFromDataURL(dataUrl);
-    //         clipboard.writeImage(img)
-    //         utools.hideMainWindow()
-    //     };
-    //     let DataURL = reader.readAsDataURL(blob);  // 将blob数据转换成DataURL数据
-    //     console.log("dd:"+DataURL); 
+    if (os.type() == 'Windows_NT') {
+        // windows 系统以 html 写入clipboard
+        clipboard.write({
+            html: "<img src=\"" + imgUrl + "\">",
+        })
+        utools.hideMainWindow()
+        return
+    } else if (os.type() == 'Darwin') {
+        // mac 系统下载网络图片到 temp 目录，然后以 NSFilenamesPboardType 的 Buffer 写入clipboard
+        var the_dir = utools.getPath('temp') + '/utoolsDoutuPlugin/';
+        var path = the_dir + 'temp.' + imgUrl.substring(imgUrl.lastIndexOf(".") + 1)
+        //检查临时目录并创建
+        fs.exists(the_dir, function (exists) {
+            if (!exists) {
+                fs.mkdir(the_dir, err => {
+                })
+            }
+        });
+        fetch(imgUrl)
+            .then(respone => respone.blob())    // 将响应体转换成blob格式数据
+            .then(blob => {
+                let reader = new FileReader();
+                reader.readAsDataURL(blob);
+                reader.onloadend = function () {
+                    let base64Data = reader.result;
+                    // 过滤data:URL
+                    base64Data = base64Data.replace(/^data:image\/\w+;base64,/, '');
+                    var dataBuffer = new Buffer(base64Data, 'base64');
+
+                    fs.writeFile(path, dataBuffer, err => {
+
+                        clipboard.writeBuffer(
+                            'NSFilenamesPboardType',
+                            Buffer.from(`
+                  <?xml version="1.0" encoding="UTF-8"?>
+                  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+                  <plist version="1.0">
+                    <array>
+                      <string>`+ path + `</string>
+                    </array>
+                  </plist>
+                `)
+                        )
+
+                    });
+                    utools.hideMainWindow()
+                };
+
+            })
+            .catch(console.error);
+    } else {
+        utools.showNotification('当前系统暂不支持', clickFeatureCode = null, silent = false)
+    }
 
 
-    // })
-    // .catch(console.error);
-    console.log(clipboard.availableFormats());
-    clipboard.writeHTML('<p><img src="http://newsget-cache.stor.sinaapp.com/a708e39e34c71cada7250feb85082ab2.png"/></p>')
 
-
-   
-    // fs.readFile('/Users/vst/Documents/Code/utools/doutu/assets/loading.gif',function(err,data){
-    //     var bitmap = new Buffer(data).toString('base64')
-    //     var bit = Buffer.from(bitmap,'base64');
-    //     let img = electron.nativeImage.createFromDataURL(bit);
-    //     console.log("img:"+img);
-    //     clipboard.writeImage(img);
-    //     utools.hideMainWindow()
-    //   });
-
-
-    // fetch(imgUrl)
-    //     .then(respone => respone.blob())    // 将响应体转换成blob格式数据
-    //     .then(blob => {
-    //         let reader = new FileReader();
-    //         reader.readAsDataURL(blob);  // 将blob数据转换成DataURL数据
-    //         reader.onloadend = function () {
-    //             let dataUrl = reader.result;
-    //             console.log("rr:"+dataUrl); // 输出DataURL数据
-    //             let img = electron.nativeImage.createFromDataURL(dataUrl);
-    //             console.log("img:"+img);
-    //             // img = img.toDataURL();
-    //             // console.log("imgTU:"+img);
-    //             clipboard.writeImage(img);
-    //             clipboard.writeBuffer('img', dataUrl)
-    //             // clipboard.write({
-    //             //     html: "<img src=\""+imgUrl+"\">",
-    //             // });
-    //             utools.hideMainWindow()
-    //         };
-           
-    //     })
-    //     .catch(console.error);
-
-        
-    // let image = new Image();
-    // image.src = imgUrl;
-    // image.onload = function () {
-    //     //图片加载完成后转换成base64字符串
-    //     let base64 = getBase64Image(image);
-    //     let img = electron.nativeImage.createFromDataURL(base64);
-    //     clipboard.writeImage(img)
-    //     utools.hideMainWindow()
-    // };
 }
+
 
 //通过canvas2d 将图片转为base64
-function getBase64Image(img) {
-    let canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    let ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0, img.width, img.height);
-    let ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
-    let dataURL = canvas.toDataURL("image/" + ext);
-    return dataURL;
-}
+// function getBase64Image(img) {
+//     let canvas = document.createElement("canvas");
+//     canvas.width = img.width;
+//     canvas.height = img.height;
+//     let ctx = canvas.getContext("2d");
+//     ctx.drawImage(img, 0, 0, img.width, img.height);
+//     let ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
+//     let dataURL = canvas.toDataURL("image/" + ext);
+//     return dataURL;
+// }
 
 matchImgUrl = str => {
     var reg = /data-original="(.*?)"/gim;
